225ddc5724a61748bacb8510227bc723c562c077
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2021-09-25 19:43:25 +0700

    fix: pre-commit hooks and testing

--- helper/error.js
+++ helper/error.js
@@ -1,22 +1,14 @@
-const errHandler = (err, req, res, next)=>{
-    let statusCode
-    let errorMessage
-    switch(err.name){
-        case'SequelizeValidationError' : 
-            statusCode = 401
-            errorMessage = {code: 401, message: err.errors[0].message}
-            break
-        case 'ParameterMissingError' :
-            statusCode = 400
-            errorMessage = {code: 400, message: err.message}
-            break
-        default :
-            statusCode = 500
-            errorMessage = {code: 500, message: 'Internal Server Error'}
-            break
-    }
+const errHandler = (err, req, res, next) => {
+  let statusCode;
+  let errorMessage;
+  switch (err.name) {
+    default:
+      statusCode = 500;
+      errorMessage = { code: 500, message: "Internal Server Error" };
+      break;
+  }
 
-    res.status(statusCode).json(errorMessage)
-}
+  res.status(statusCode).json(errorMessage);
+};
 
-module.exports = errHandler
+module.exports = errHandler;
--- router/index.js
+++ router/index.js
@@ -1,9 +1,5 @@
-const router = require('express').Router()
-const UserPremium = require('../controller')
+const router = require("express").Router();
+const UserPremium = require("../controller");
 
-router.get('/', (req, res,) => {
-    res.send('user premium service');
-});
-
-router.post('/user/premium', UserPremium.register)
-module.exports = router
+router.post("/user/premium", UserPremium.register);
+module.exports = router;
